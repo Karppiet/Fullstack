d2bc42c9491d61e27b0d8b658293ae4e9afbec53
karppi <karppi@min.com>
2025-10-29 12:51:33 +0200

    App.get added

--- WS3/example/ws3-api/server.js
+++ WS3/example/ws3-api/server.js
@@ -13,5 +13,8 @@ app.post("/api/todos", (req, res) => {
   todos.push(todo);
   res.status(201).json(todo);
 });
+app.get("/", (req, res) => {
+  res.send("✅ API is running! Try /api/hello or /api/todos");
+});
 app.listen(PORT, () => console.log("API on http://localhost:" + PORT));
 
